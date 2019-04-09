512c1449d244129f4c8f0891047bf331a01b7d64
uzig305 <zjt_james@163.com>
2019-04-09 11:32:53 +0800

    init

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+/**
+ * created by Zheng Jiateng on 2019/4/9.
+ */
+
+public class Main {
+    public static void main(String[] args) {
+//        Scanner in = new Scanner(System.in);
+//        while (in.hasNextInt()) { // 注意while处理多个case
+//            int b = in.nextInt();
+//            System.out.println(b);
+//
+//        }
+        String a = "555";
+        System.out.println(a.matches("[a-zA-Z0-9]*"));
+    }
+}
