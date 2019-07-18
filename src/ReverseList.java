3e5fccbdc965d26ba83446c0d94d1126d0ff5468
uzig305 <zjt_james@163.com>
2019-07-18 15:57:57 +0800

    if判断设置新头是易漏点

--- src/ReverseList.java
+++ src/ReverseList.java
@@ -5,26 +5,28 @@ import base.ListNode;
  */
 
 /**
- * 反转链表： 三个指针 一个指向当前节点，一个指向前一个，一直提前保存后一个节点，防止链表断裂
- *
- * 输入一个链表，反转链表后，输出新链表的表头。
+ * 反转链表：输入一个链表，反转链表后，输出新链表的表头。
  * a->b->c->d->null
  * null<-a<-b<-c<-d
+ *
+ * 要点：三个指针 一个指向当前节点，一个指向前一个，一个提前保存后一个节点，防止链表断裂
+ * if判断设置新头是易漏点
  */
 public class ReverseList {
     public ListNode ReverseList(ListNode head) {
-        ListNode reversedHead = null;
-        ListNode node = head;
-        ListNode previous = null;
-        while (node != null) {
-            if (node.next == null) {
-                reversedHead = node;
+        ListNode previos = null;
+        ListNode next = null;
+        ListNode newHead = null;
+        ListNode cur = head;
+        while (cur != null) {
+            if (cur.next == null) { // 这个if判断设置新头是易漏点
+                newHead = cur;
             }
-            ListNode next = node.next;
-            node.next = previous;
-            previous = node;
-            node = next;
+            next = cur.next;
+            cur.next = previos;
+            previos = cur;
+            cur = next;
         }
-        return reversedHead;
+        return newHead;
     }
 }
